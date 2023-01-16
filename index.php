4b6f9408c8602e06849c32a79aae9cd640d5c0de
Sajidur Rahman <shajidur171@gmail.com>
2023-01-16 17:45:14 +0600

    Last

--- index.php
+++ index.php
@@ -5,22 +5,63 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form</title>
+    
+  <link rel="manifest" href="site.webmanifest" />
+  <link rel="apple-touch-icon" href="icon.png" />
+  <!-- Place favicon.ico in the root directory -->
+
+  <!-- bootstrap css -->
+  <link rel="stylesheet" href="css/bootstrap.min.css" />
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+
+  <!-- Fontawesome CDN -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- icofont -->
+  <link rel="stylesheet" href="css/icofont.min.css" />
+  <link rel="stylesheet" href="css/normalize.css" />
+  <link rel="stylesheet" href="css/responsive-testimonials.css" />
+  <link rel="stylesheet" href="css/main.css" />
+  <link rel="stylesheet" href="style.css" />
 </head>
 <body>
 
-<form action="#">
-<select name="year" id="year">
+<div class="container">
+<div class="row">
+<?php 
 
-    <?php
-    $year = 2000;
-     while($year <= 2050){ ?>
-        <option value="<?php echo $year;?>"><?php echo $year;?></option>
-    <?php 
-    $year++;
-    }
-    ?>
-</select>
-</form>
+$query = 1;
 
+do{ 
+  $query++;
+  ?>
+  <div class="col-md-4">
+  <div class="box1">
+    <h3>Blog Title Goes Here</h3>
+    <img src="./img/post.jpg" alt="">
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis eaque repellendus, aliquid, inventore sint at repellat pariatur facere libero enim unde temporibus molestias obcaecati, accusantium vitae ex veniam! Quaerat, nostrum!</p>
+  </div>
+</div>
+
+<?php 
+} while($query <= 12)
+?>
+  
+
+</div>
+</div>
+
+<script src="js/vendor/modernizr-3.11.2.min.js"></script>
+  <script src="js/jquery-3.6.1.min.js"></script>
+  <!-- boostrap js -->
+  <script src="js/bootstrap.bundle.min.js"></script>
+  <script src="js/plugins.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+  <script src="js/countMe.min.js"></script>
+  <script src="js/responsive-testimonials.js"></script>
+  <script src="js/main.js"></script>
+  <script src="https://www.google-analytics.com/analytics.js" async></script>
 </body>
 </html>
