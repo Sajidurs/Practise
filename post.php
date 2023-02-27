12aa117783ca81b7260f54750eeaf1b3fe1444e8
Sajidur Rahman <shajidur171@gmail.com>
2023-02-27 23:49:54 +0600

    Adding a new ternary example

--- post.php
+++ post.php
@@ -181,5 +181,11 @@ $value = 1;
 $checked = true;
 $unchecked = false;
 
-$output = ($value = 1) ? "Checked" : "Unchecked";
+$output = ($value = 0) ? "Checked" : "Unchecked";
 echo $output;
+
+echo "<hr>";
+
+$a = 10;
+$b = $a > 15 ? 20 : 5;
+print ("Value of b is " . $b);
